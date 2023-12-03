cc4370407b58677e086a5942ff2f51c86f7ac8e1
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-12-03 10:24:30 +0100

    add Vector type

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
 
+* `Vector2<T>` types with `Vec2` and `IVec2` aliases
 * Support `no_std` (by disabling the `std` feature flag)
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,59 @@
 //! # Features
 //!
 //! `std`: *(enabled by default)* enable use of the standard library. Must be disabled for `no_std` crates.
+
+/// Alias for `Vector<f32>`
+pub type Vec2 = Vector2<f32>;
+
+/// Alias for `Vector<i32>`
+pub type IVec2 = Vector2<i32>;
+
+/// Vector type, generic over its scalar type `T`
+#[allow(clippy::exhaustive_structs)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub struct Vector2<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> Vector2<T> {
+    pub const fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T> From<[T; 2]> for Vector2<T> {
+    fn from([x, y]: [T; 2]) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T> From<Vector2<T>> for [T; 2] {
+    fn from(Vector2 { x, y }: Vector2<T>) -> Self {
+        [x, y]
+    }
+}
+
+impl<T> From<(T, T)> for Vector2<T> {
+    fn from((x, y): (T, T)) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T> From<Vector2<T>> for (T, T) {
+    fn from(Vector2 { x, y }: Vector2<T>) -> Self {
+        (x, y)
+    }
+}
+
+impl Vector2<f32> {
+    pub const ZERO: Self = Self::new(0., 0.);
+    pub const X: Self = Self::new(1., 0.);
+    pub const Y: Self = Self::new(0., 1.);
+}
+
+impl Vector2<i32> {
+    pub const ZERO: Self = Self::new(0, 0);
+    pub const X: Self = Self::new(1, 0);
+    pub const Y: Self = Self::new(0, 1);
+}
