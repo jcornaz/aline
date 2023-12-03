b0e347362ef22991d60d2c74f7832cc1eb6646af
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-12-03 10:11:25 +0100

    add `std` feature (enabled by default)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
 
+* Support `no_std` (by disabling the `std` feature flag)
+
 
 [Unreleased]: https://github.com/jcornaz/aline/compare/...HEAD
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 //! A simple 2d linear algebra library suitable for `no_std`
+//!
+//! # Features
+//!
+//! `std`: *(enabled by default)* enable use of the standard library. Must be disabled for `no_std` crates.
